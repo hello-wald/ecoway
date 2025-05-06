6e80e4ac78549d7035eb245ffeeb1a62ce10cb68
Dxtree <qwertys16051@gmail.com>
2025-05-06 13:23:17 +0700

    add persistent layer

---
+++ backend/eco-backend/model/destinationModel.ts
@@ -0,0 +1,7 @@
+export interface DestinationModel {
+    destination_id: string;
+    destination_name: string;
+    latitude: number;
+    longitude: number;
+  }
+  
---
+++ backend/eco-backend/model/tripTransactionModel.ts
@@ -0,0 +1,7 @@
+export interface TripTransactionModel {
+    customer_id: string;
+    driver_id: string;
+    destination_id: string;
+    trip_date: Date;
+    trip_point: number;
+  }
