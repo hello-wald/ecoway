3dc46d45a62b62d0e96f5b6bef8833d18434132f
Dxtree <qwertys16051@gmail.com>
2025-05-08 14:34:28 +0700

    feat: add ongoingTransaction object

---
+++ backend/eco-backend/model/onGoingTransactionModel.ts
@@ -0,0 +1,9 @@
+export interface onGoingTransactionModel{
+    on_transaction_id: string
+    driver_id:string
+    passenger_id:string
+    vehicle_id:string
+    destination_id: string
+    driver_location: Location
+    passenger_location: Location
+}
