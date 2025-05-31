9a78ad9d5ba40a83bea8d88a51f07e318f396871
Catherine Alyssa <catherinealyssar@gmail.com>
2025-05-31 18:51:46 +0700

    feat: Simplify theme index exports by using wildcard exports

--- frontend/theme/index.ts
+++ frontend/theme/index.ts
@@ -1,8 +1,7 @@
-import { BorderRadius, BorderWidth, Shadow } from "./outlines";
-import { IconSize, Spacing } from "./sizing";
-import { Font } from "./text-style";
-import { ThemeProvider } from "./provider/theme-provider";
-import { useTheme } from "./hooks/useTheme";
-import { createAuthStyles } from "./styles/auth-styles";
-
-export { BorderRadius, BorderWidth, Shadow, Spacing, Font, IconSize, useTheme, ThemeProvider, createAuthStyles };
+export * from "./outlines";
+export * from "./sizing";
+export * from "./text-style";
+export * from "./colors";
+export * from "./provider/theme-provider";
+export * from "./hooks/useTheme";
+export * from "./styles/auth-styles";
